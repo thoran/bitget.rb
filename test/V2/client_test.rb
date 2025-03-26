82eb2e7608d7ddf3cee0b81e66a84fa943918c4c
thoran <code@thoran.com>
2025-03-26 23:20:49 +1100

    0.1.1: Add missing tests for supplying an argument to the coins endpoint.

--- test/V2/client_test.rb
+++ test/V2/client_test.rb
@@ -11,13 +11,27 @@ describe Bitget::V2::Client do
   end
 
   describe "#spot_public_coins" do
-    it "retrieves a list of all coins" do
-      VCR.use_cassette('v2/spot/public/coins') do
-        response = client.spot_public_coins
-        _(response).must_include('data')
-        _(response['data'].first).must_include('coinId')
-        _(response['data'].first).must_include('coin')
-        assert_operator response['data'].count, :>, 1500
+    context "when a coin is NOT supplied" do
+      it "retrieves a list of all coins" do
+        VCR.use_cassette('v2/spot/public/coins-when_coin_is_not_supplied') do
+          response = client.spot_public_coins
+          _(response).must_include('data')
+          _(response['data'].first).must_include('coinId')
+          _(response['data'].first).must_include('coin')
+          assert_operator response['data'].count, :>, 1500
+        end
+      end
+    end
+
+    context "when a coin IS supplied" do
+      it "retrieves one coin" do
+        VCR.use_cassette('v2/spot/public/coins-when_coin_is_supplied') do
+          response = client.spot_public_coins(coin: 'BTC')
+          _(response).must_include('data')
+          _(response['data'].first).must_include('coinId')
+          _(response['data'].first).must_include('coin')
+          _(response['data'].count).must_equal(1)
+        end
       end
     end
   end
